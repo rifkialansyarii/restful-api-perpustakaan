b5b3782c68c0b6258a4949ed863044de03b07cfe
rifkialansyarii <alansyari3431@gmail.com>
2026-04-01 08:37:15 +0800

    refactor: Refactor pemanggilan middleware

--- public/index.php
+++ public/index.php
@@ -4,45 +4,46 @@ require_once __DIR__ . "/../vendor/autoload.php";
 require_once __DIR__ . "/../bootstrap.php";
 
 use Base\Router;
+use App\Middlewares\AuthMiddleware;
 
 /**
  * Default route is /api/
  * You should access all of this route using /api/<endpoint>
  */
 
-Router::add("GET", "/borrows", "App\Controllers\BorrowController", "index", [App\Middlewares\AuthMiddleware::class]);
-Router::add("GET", "/borrows/([0-9]+)", "App\Controllers\BorrowController", "show", [App\Middlewares\AuthMiddleware::class]);
-Router::add("POST", "/borrows", "App\Controllers\BorrowController", "store", [App\Middlewares\AuthMiddleware::class]);
-Router::add("PATCH", "/borrows/([0-9]+)", "App\Controllers\BorrowController", "update", [App\Middlewares\AuthMiddleware::class]);
-Router::add("DELETE", "/borrows/([0-9]+)", "App\Controllers\BorrowController", "destroy", [App\Middlewares\AuthMiddleware::class]);
+Router::add("GET", "/borrows", "App\Controllers\BorrowController", "index", [AuthMiddleware::class]);
+Router::add("GET", "/borrows/([0-9]+)", "App\Controllers\BorrowController", "show", [AuthMiddleware::class]);
+Router::add("POST", "/borrows", "App\Controllers\BorrowController", "store", [AuthMiddleware::class]);
+Router::add("PATCH", "/borrows/([0-9]+)", "App\Controllers\BorrowController", "update", [AuthMiddleware::class]);
+Router::add("DELETE", "/borrows/([0-9]+)", "App\Controllers\BorrowController", "destroy", [AuthMiddleware::class]);
 
 
 Router::add("GET", "/books", "App\Controllers\BookController", "index");
 Router::add("GET", "/books/([0-9]+)", "App\Controllers\BookController", "show");
-Router::add("POST", "/books", "App\Controllers\BookController", "store", [App\Middlewares\AuthMiddleware::class]);
-Router::add("PATCH", "/books/([0-9]+)", "App\Controllers\BookController", "update", [App\Middlewares\AuthMiddleware::class]);
-Router::add("DELETE", "/books/([0-9]+)", "App\Controllers\BookController", "destroy", [App\Middlewares\AuthMiddleware::class]);
+Router::add("POST", "/books", "App\Controllers\BookController", "store", [AuthMiddleware::class]);
+Router::add("PATCH", "/books/([0-9]+)", "App\Controllers\BookController", "update", [AuthMiddleware::class]);
+Router::add("DELETE", "/books/([0-9]+)", "App\Controllers\BookController", "destroy", [AuthMiddleware::class]);
 
 
 Router::add("GET", "/authors", "App\Controllers\AuthorController", "index");
 Router::add("GET", "/authors/([0-9]+)", "App\Controllers\AuthorController", "show");
-Router::add("POST", "/authors", "App\Controllers\AuthorController", "store", [App\Middlewares\AuthMiddleware::class]);
-Router::add("PATCH", "/authors/([0-9]+)", "App\Controllers\AuthorController", "update", [App\Middlewares\AuthMiddleware::class]);
-Router::add("DELETE", "/authors/([0-9]+)", "App\Controllers\AuthorController", "destroy", [App\Middlewares\AuthMiddleware::class]);
+Router::add("POST", "/authors", "App\Controllers\AuthorController", "store", [AuthMiddleware::class]);
+Router::add("PATCH", "/authors/([0-9]+)", "App\Controllers\AuthorController", "update", [AuthMiddleware::class]);
+Router::add("DELETE", "/authors/([0-9]+)", "App\Controllers\AuthorController", "destroy", [AuthMiddleware::class]);
 
 
 Router::add("GET", "/categories", "App\Controllers\CategoryController", "index");
 Router::add("GET", "/categories/([0-9]+)", "App\Controllers\CategoryController", "show");
-Router::add("POST", "/categories", "App\Controllers\CategoryController", "store", [App\Middlewares\AuthMiddleware::class]);
-Router::add("PATCH", "/categories/([0-9]+)", "App\Controllers\CategoryController", "update", [App\Middlewares\AuthMiddleware::class]);
-Router::add("DELETE", "/categories/([0-9]+)", "App\Controllers\CategoryController", "destroy", [App\Middlewares\AuthMiddleware::class]);
+Router::add("POST", "/categories", "App\Controllers\CategoryController", "store", [AuthMiddleware::class]);
+Router::add("PATCH", "/categories/([0-9]+)", "App\Controllers\CategoryController", "update", [AuthMiddleware::class]);
+Router::add("DELETE", "/categories/([0-9]+)", "App\Controllers\CategoryController", "destroy", [AuthMiddleware::class]);
 
 
-Router::add("GET", "/users", "App\Controllers\UserController", "index", [App\Middlewares\AuthMiddleware::class]);
-Router::add("GET", "/users/([0-9]+)", "App\Controllers\UserController", "show", [App\Middlewares\AuthMiddleware::class]);
-Router::add("POST", "/users", "App\Controllers\UserController", "store", [App\Middlewares\AuthMiddleware::class]);
-Router::add("PATCH", "/users/([0-9]+)", "App\Controllers\UserController", "update", [App\Middlewares\AuthMiddleware::class]);
-Router::add("DELETE", "/users/([0-9]+)", "App\Controllers\UserController", "destroy", [App\Middlewares\AuthMiddleware::class]);
+Router::add("GET", "/users", "App\Controllers\UserController", "index", [AuthMiddleware::class]);
+Router::add("GET", "/users/([0-9]+)", "App\Controllers\UserController", "show", [AuthMiddleware::class]);
+Router::add("POST", "/users", "App\Controllers\UserController", "store", [AuthMiddleware::class]);
+Router::add("PATCH", "/users/([0-9]+)", "App\Controllers\UserController", "update", [AuthMiddleware::class]);
+Router::add("DELETE", "/users/([0-9]+)", "App\Controllers\UserController", "destroy", [AuthMiddleware::class]);
 
 Router::add("POST", "/login", "App\Controllers\AuthController", "authenticate");
 
